bf4f3c0da4ac7e198a0c477fb93dae654367dffd
teto72 <tsubasa.k.729@gmail.com>
2020-07-20 06:56:37 +0900

    add:contact.index
    お問い合わせフォームの実装

---
+++ JavaScript/main.js
@@ -0,0 +1,21 @@
+// -------------------
+// スムーススクロール
+// -------------------
+$(function(){
+  $('a[href^="#"]').click(function(){
+    let speed = 500;
+    let href= $(this).attr("href");
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    let position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, 400, "swing");
+    return false;
+  });
+});
+
+// ------------------
+// ページ遷移時のフェードイン時間
+// ------------------
+$('body').hide();
+$(window).on('load', function () {
+  $('body').fadeIn(2000); // fadeInにかける時間
+});
--- main.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const el = document.querySelector('h1');
-  console.log(el);
-});
