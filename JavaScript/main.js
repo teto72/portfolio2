5c2f08e4b3076ad651ce484f4adac8c2691478fa
teto72 <tsubasa.k.729@gmail.com>
2020-07-23 01:27:35 +0900

    add:skill詳細部
    各タイトルへテキストアニメーション追加

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -19,3 +19,26 @@ $('body').hide();
 $(window).on('load', function () {
   $('body').fadeIn(2000); // fadeInにかける時間
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  const els = document.querySelectorAll('#animate-title');
+  const cb = function (entries, observer) {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              const ta = new TextAnimation(entry.target);
+              ta.animate();
+              observer.unobserve(entry.target);
+          } else {
+          }
+      });
+  };
+  const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0
+  };
+  const io = new IntersectionObserver(cb, options);
+  els.forEach(el => io.observe(el));
+});
