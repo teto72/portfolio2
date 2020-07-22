9f0fedb186d3ce7e0142fb975e4c85ebdbb0b969
teto72 <tsubasa.k.729@gmail.com>
2020-07-23 03:39:49 +0900

    add:text-animation
    テキストアニメーション追加

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -20,7 +20,6 @@ $(window).on('load', function () {
   $('body').fadeIn(2000); // fadeInにかける時間
 });
 
-
 document.addEventListener('DOMContentLoaded', function () {
 
   const els = document.querySelectorAll('#animate-title');
@@ -42,3 +41,17 @@ document.addEventListener('DOMContentLoaded', function () {
   const io = new IntersectionObserver(cb, options);
   els.forEach(el => io.observe(el));
 });
+
+// -----------------
+// テキストフェードイン
+// -----------------
+$(window).scroll(function (){
+  $('.fadein').each(function(){
+      let elemPos = $(this).offset().top,
+          scroll = $(window).scrollTop(),
+          windowHeight = $(window).height();
+        if (scroll > elemPos - windowHeight + 100){
+            $(this).addClass('scrollin');
+          }
+      });
+  });
